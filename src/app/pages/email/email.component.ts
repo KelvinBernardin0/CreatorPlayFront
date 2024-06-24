aa027d0c1655e9eadf0322ff0d2f250ee2d8081a
Kelvin Bernardino <kelvin.bernardino@compasso.com.br>
2024-06-24 14:12:20 -0300

    deploy homol

--- src/app/pages/email/email.component.ts
+++ src/app/pages/email/email.component.ts
@@ -157,6 +157,25 @@ export class EmailComponent {
 
     event.preventDefault();
   }
+
+  // Função para atualizar o HTML após as mudanças
+  atualizarHTML() {
+
+    const headerContainer = document.getElementById('header-container');
+    const contentContainer = document.getElementById('content-container');
+    const footerContainer = document.getElementById('footer-container');
+
+    if (headerContainer && contentContainer && footerContainer) {
+      const headerHTML = headerContainer.innerHTML;
+      const contentHTML = contentContainer.innerHTML;
+      const footerHTML = footerContainer.innerHTML;
+
+      // Atualiza o conteúdo HTML bruto e seguro para cada parte
+      this.headerHTML = this.sanitizer.bypassSecurityTrustHtml(headerHTML);
+      this.contentHTML = this.sanitizer.bypassSecurityTrustHtml(contentHTML);
+      this.footerHTML = this.sanitizer.bypassSecurityTrustHtml(footerHTML);
+    }
+  }
   //---------------- FIM DO FUNCIONAMENTO DO HTML ----------------
 
 
@@ -424,6 +443,7 @@ export class EmailComponent {
       const selectedContents = range.extractContents();
       const span = document.createElement('span');
       span.style.color = cor;
+
   
       span.appendChild(selectedContents);
       range.insertNode(span);
@@ -433,105 +453,83 @@ export class EmailComponent {
     }
   }
   
-  
 
- // Função para atualizar o HTML após as mudanças
- atualizarHTML() {
-  const headerContainer = document.getElementById('header-container');
-  const contentContainer = document.getElementById('content-container');
-  const footerContainer = document.getElementById('footer-container');
 
-  if (headerContainer && contentContainer && footerContainer) {
-    const headerHTML = headerContainer.innerHTML;
-    const contentHTML = contentContainer.innerHTML;
-    const footerHTML = footerContainer.innerHTML;
+  // Aplica o estilo selecionado ao texto selecionado
+  aplicaEstiloSelecionado(event: Event) {
+    const selectedValue = (event.target as HTMLSelectElement).value;
+    if (!selectedValue) return; // Retorna se nenhum valor estiver selecionado
 
-    // Atualiza o conteúdo HTML bruto e seguro para cada parte
-    this.headerHTML = this.sanitizer.bypassSecurityTrustHtml(headerHTML);
-    this.contentHTML = this.sanitizer.bypassSecurityTrustHtml(contentHTML);
-    this.footerHTML = this.sanitizer.bypassSecurityTrustHtml(footerHTML);
+    const selection = window.getSelection();
+    if (!selection || selection.rangeCount === 0) return; // Retorna se não houver seleção
+
+    this.saveState(); // Salvar o estado antes de fazer a alteração
+
+    const range = selection.getRangeAt(0);
+    const selectedContents = range.extractContents();
+
+    const span = document.createElement('span');
+    switch (selectedValue) {
+      case 'bold':
+        span.style.fontWeight = 'bold';
+        break;
+      case 'italic':
+        span.style.fontStyle = 'italic';
+        break;
+      case 'underline':
+        span.style.textDecoration = 'underline';
+        break;
+      default:
+        break;
+    }
+
+    span.appendChild(selectedContents);
+    range.insertNode(span);
+
+    // Atualiza o HTML após aplicar o estilo
+    this.atualizarHTML();
   }
-}
-
-
-
-
-  
-aplicaEstiloSelecionado(event: Event) {
-  const selectedValue = (event.target as HTMLSelectElement).value;
-  if (!selectedValue) return; // Retorna se nenhum valor estiver selecionado
-
-  const selection = window.getSelection();
-  if (!selection || selection.rangeCount === 0) return; // Retorna se não houver seleção
-
-  const range = selection.getRangeAt(0);
-  const selectedContents = range.extractContents();
-
-  const span = document.createElement('span');
-  switch (selectedValue) {
-    case 'bold':
-      span.style.fontWeight = 'bold';
-      break;
-    case 'italic':
-      span.style.fontStyle = 'italic';
-      break;
-    case 'underline':
-      span.style.textDecoration = 'underline';
-      break;
-    default:
-      break;
-  }
-
-  span.appendChild(selectedContents);
-  range.insertNode(span);
-
-  this.saveState(); // Salva o estado antes de fazer a alteração
-
-  this.atualizarHTML(); // Atualiza o HTML editável
-}
 
 
 
   // INSERIR LINK
-// INSERIR LINK
-inserirLink() {
-  const selection = window.getSelection();
-  if (selection && selection.rangeCount > 0) {
-    const range = selection.getRangeAt(0);
-    const container = range.commonAncestorContainer as HTMLElement;
+  inserirLink() {
+    const selection = window.getSelection();
+    if (selection && selection.rangeCount > 0) {
+      const range = selection.getRangeAt(0);
+      const container = range.commonAncestorContainer as HTMLElement;
 
-    const imgElement = container.querySelector('img');
-    if (imgElement) {
-      let link = prompt('Digite o URL do link:');
-      if (link) {
-        // Verificar se o link é relativo e convertê-lo para absoluto
-        if (!link.match(/^[a-zA-Z][a-zA-Z\d+\-.]*:/)) {
-          link = 'https://' + link;
+      const imgElement = container.querySelector('img');
+      if (imgElement) {
+        let link = prompt('Digite o URL do link:');
+        if (link) {
+          // Verificar se o link é relativo e convertê-lo para absoluto
+          if (!link.match(/^[a-zA-Z][a-zA-Z\d+\-.]*:/)) {
+            link = 'https://' + link;
+          }
+
+          const aElement = document.createElement('a');
+          aElement.href = link;
+          aElement.target = '_blank';
+          aElement.title = link; // Define o tooltip para exibir o link
+
+          // Clonar a imagem e adicionar ao link
+          const imgClone = imgElement.cloneNode(true);
+          aElement.appendChild(imgClone);
+
+          this.saveState(); // Salvar o estado antes de fazer a alteração
+
+          // Substituir a imagem original pelo link com a imagem clonada
+          imgElement.parentNode?.replaceChild(aElement, imgElement);
+
+          // Atualizar o HTML
+          this.atualizarHTML();
         }
-
-        const aElement = document.createElement('a');
-        aElement.href = link;
-        aElement.target = '_blank';
-        aElement.title = link; // Define o tooltip para exibir o link
-
-        // Clonar a imagem e adicionar ao link
-        const imgClone = imgElement.cloneNode(true);
-        aElement.appendChild(imgClone);
-
-        this.saveState(); // Salvar o estado antes de fazer a alteração
-
-        // Substituir a imagem original pelo link com a imagem clonada
-        imgElement.parentNode?.replaceChild(aElement, imgElement);
-
-        // Atualizar o HTML
-        this.atualizarHTML();
+      } else {
+        alert('Por favor, selecione uma imagem primeiro.');
       }
-    } else {
-      alert('Por favor, selecione uma imagem primeiro.');
     }
   }
-}
-
 
   //---------------- FIM CARREGAR TEXTOS ----------------
 
